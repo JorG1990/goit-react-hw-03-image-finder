ab693fd0a89b44028d5fc6bf078547c06865fd1e
JorG1990 <jorge  garcia>
2023-08-20 22:53:27 -0500

    add app.jsx ahd app.styled.jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,99 @@
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Searchbar from "../Searchbar/Searchbar";
+import Modal from "../Modal/Modal";
+import Button from "../Button/Button";
+import ImageGallery from "../ImageGallery/ImageGallery";
+import ImageGalleryItem from "../ImageGalleryItem/ImageGalleryItem";
+import MyLoader from "../Loader/Loader";
+import { GalleryContainer, SearchbarContainer,ButtonContainer } from "./App.styled";
+
+const keyApi = "38957997-b8acc7a81f773178d46be80fd";
+
+function App() {
+  const [ images, setImages ] = useState([]);
+  const [ searchQuery, setSearchQuery ] = useState([]);
+  const [ page, setPage ] = useState([]);
+  const [ isLoading, setIsLoading ] = useState([]);
+  const [ showModal, setShowModal ] = useState([]);
+  const [ selectedImage, setSelectedImage ] = useState([]);
+  const [ showMoreButton, setShowMoreButton ] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = () => {
+      if (!searchQuery.trim()) {
+        setImages([]);
+        setShowMoreButton(false);
+        return;
+      };
+
+      const url = `https://pixabay.com/api/?key=${keyApi}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12`;
+
+      setIsLoading(true);
+      axios
+      .get(url)
+      .then((response) => {
+        const { hits } = response.data;
+        setImages((prevImages) => [...prevImages, ...hits ]);
+        setIsLoading(false);
+        setShowMoreButton(hits.length === 12);
+      })
+      .catch((error) => console.log(error));
+    };
+
+    fetchImages();
+  }, [ searchQuery, page ]);
+
+  const openModal = (imageurl) => {
+    setShowModal(true);
+    setSelectedImage(imageurl);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setSelectedImage(null);
+  }
+
+  const handleSearchbarSubmit = (query) => {
+
+    if (!query.trim()) {
+      setSearchQuery('');
+      return;
+    }
+
+    setSearchQuery(query);
+    setPage(1);
+    setImages([]);
+    setShowMoreButton(false);
+  };
+
+  return (
+    <>
+      <SearchbarContainer>
+        <Searchbar onSubmit={handleSearchbarSubmit} />
+      </SearchbarContainer>
+      <GalleryContainer>
+        {images.map((image, index) => (
+          <ImageGalleryItem key={image.id + '_' + index} image={image} onImageClick={openModal} />
+        ))}
+        {isLoading ? (
+          <MyLoader />
+        ) : (
+          <ImageGallery images={images} onImageClick={openModal} />
+        )}
+        {showModal && <Modal imageUrl={ selectedImage} closeModal={closeModal} />}
+      </GalleryContainer>
+      {showMoreButton && (
+        <div>
+          <ButtonContainer>
+            <Button onClick={() => setPage(page +1)} disabled={isLoading} showMoreButton={true} />
+          </ButtonContainer>
+        </div>
+      )}
+
+    </>
+  )
+}
+
+export default App;
