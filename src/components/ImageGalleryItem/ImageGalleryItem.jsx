c4660fbc4763355566cda967be15c6c13648bc83
JorG1990 <jorge  garcia>
2023-08-20 23:55:30 -0500

    add ImageGalleryItem.jsx ahd ImageGalleryItem.styled.jsx

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,26 @@
+
+import PropTypes from "prop-types";
+import React from "react";
+import { Li } from "./ImageGalleryItem.styled";
+
+function ImageGalleryItem({ image, onImageClick }) {
+  return (
+    <Li className="gallery-item">
+      <img src={ image.webformatURL }
+      alt={ image.tags }
+      onClick={() => onImageClick(image.largeImageURL )} />
+    </Li>
+  );
+}
+
+ImageGalleryItem.propTypes = {
+  image: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    webformatURL: PropTypes.string.isRequired,
+    tags: PropTypes.string.isRequired,
+    largeImageURL: PropTypes.string.isRequired,
+  }).isRequired,
+  onImageClick: PropTypes.func.isRequired,
+};
+
+export default ImageGalleryItem;
