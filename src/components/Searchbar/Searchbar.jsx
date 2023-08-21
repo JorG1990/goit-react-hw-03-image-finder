846ae78dbebd70d237c2a8b40aa74fd339e2632f
JorG1990 <jorge  garcia>
2023-08-21 01:21:30 -0500

    add Searchbar.jsx ahd Searchbar.styled.jsx

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,40 @@
+
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { MdSearch } from "react-icons/md";
+import { Header, SearchForm, Input, SearchButton } from "./Searchbar.styled";
+
+function Searchbar({ onSubmit }) {
+  const [query, setQuery] = useState('');
+
+  const handleChange = e=> {
+    setQuery(e.currentTarget.value);
+  };
+
+  const handleSumit = e => {
+    e.preventDefault();
+
+    onSubmit(query);
+    reset();
+  };
+
+  const reset = () => {
+    setQuery('');
+  };
+  return (
+    <Header>
+      <SearchForm onSubmit={ handleSumit }>
+        <SearchButton type="submit">
+          <MdSearch style={{ width: 20, height: 20}} />
+        </SearchButton>
+        <Input type="text" name="query" value={ query } onChange={ handleChange } autoComplete="off" autoFocus placeholder="Search images and photos" />
+      </SearchForm>
+    </Header>
+  );
+}
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func,
+};
+
+export default Searchbar;
